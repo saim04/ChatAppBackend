2305244a4574853c1abbabbcdda68b1a22329135
unknown <saimfayyaz124@gmail.com>
2025-08-08 23:16:30 +0500

    first commit

---
+++ socket/socket.js
@@ -0,0 +1,39 @@
+const { Server } = require("socket.io");
+const http = require("http");
+const express = require("express");
+
+const app = express();
+
+const server = http.createServer(app);
+
+const userSocketMap = {};
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
+
+const getReceiverSocketId = (receiverId) => {
+  return userSocketMap[receiverId];
+};
+
+io.on("connection", (socket) => {
+  console.log("a user connected", socket.id);
+  console.log("QUERY OBJ", socket.handshake.query);
+
+  const userId = socket.handshake.query.userId;
+
+  if (userId != "undefined") userSocketMap[userId] = socket.id;
+
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected", socket.id);
+    delete userSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  });
+});
+
+module.exports = { app, io, server, getReceiverSocketId };
