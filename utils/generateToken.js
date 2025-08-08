074ee496deaba3e83e2540bcdd7434d076e77ca6
unknown <saimfayyaz124@gmail.com>
2025-08-09 00:11:44 +0500

    Updates

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -8,7 +8,7 @@ const generateTokenAndSetCookie = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 15 * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    secure: true,
+    secure: false,
     sameSite: "None",
   });
 };
