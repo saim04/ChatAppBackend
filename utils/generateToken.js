27f1b01414fec2dad2663d4d5159880d4857bd03
unknown <saimfayyaz124@gmail.com>
2025-08-09 00:16:46 +0500

    UPdates

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -9,7 +9,7 @@ const generateTokenAndSetCookie = (userId, res) => {
     maxAge: 15 * 24 * 60 * 60 * 1000,
     httpOnly: true,
     secure: false,
-    sameSite: "None",
+    sameSite: "lax",
   });
 };
 
